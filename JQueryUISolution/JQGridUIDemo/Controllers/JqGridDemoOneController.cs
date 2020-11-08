9a3cd40a8c129174cadf327006b493ef73936307
J.C <jeter.chen@frog-jump.com>
2020-11-08 21:27:36 +0800

    finish frontend CRUD

---
+++ JQueryUISolution/JQGridUIDemo/Controllers/JqGridDemoOneController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQGridUIDemo.Controllers
+{
+    public class JqGridDemoOneController : Controller
+    {
+        // GET: JqGridDemoOne
+        public ActionResult jqGridFirst()
+        {
+            return View();
+        }
+    }
+}
+
